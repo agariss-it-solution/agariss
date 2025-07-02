58103c590aae4797cc911240a2236a24d45b7792
agariss-it-solution <agarissitsolution@gmail.com>
2025-07-02 16:42:43 +0530

    Update blog.js

--- backend/controller/blog.js
+++ backend/controller/blog.js
@@ -16,7 +16,7 @@ const createBlog = async (req, res) => {
       });
     }
 
-    const imageUrl = `http://localhost:3001/uploads/${req.file.filename}`;
+    const imageUrl = `http://31.97.63.245:3001/uploads/${req.file.filename}`;
 
     const blog = await blogSchema.create({
       blog_images: imageUrl,
@@ -46,7 +46,7 @@ const updateBlog = async (req, res) => {
   try {
     const { blog_title, date, blog_name } = req.body;
     const blog_image = req.file?.filename;
-    const imageUrl = `http://localhost:3001/uploads/${blog_image}`;
+    const imageUrl = `http://31.97.63.245:3001/uploads/${blog_image}`;
     const updatedBlog = await blogSchema.findByIdAndUpdate(
       req.params.id,
       {
@@ -122,7 +122,7 @@ const createBlogDetails = async (req, res) => {
     const blogImage = req.files?.blog_image?.[0]?.filename || "";
     const videoFile = req.files?.video_file?.[0]?.filename || "";
 
-    const baseUrl = "http://localhost:3001/uploads/";
+    const baseUrl = "http://31.97.63.245:3001/uploads/";
 
     // ✅ Step 2: Create blog details using existing blogId
     const blogDetail = await blogModel.create({
@@ -179,7 +179,7 @@ const updateBlogDetails = async (req, res) => {
     const blogImage = req.files?.blog_image?.[0]?.filename;
     const videoFile = req.files?.video_file?.[0]?.filename;
 
-    const baseUrl = "http://localhost:3001/uploads/";
+    const baseUrl = "http://31.97.63.245:3001/uploads/";
 
     // ✅ Update only the provided fields
     if (bannerImage) blogDetail.banner_images = baseUrl + bannerImage;
