37f9261534dc20b5377a75dd8064f9cdcd4eb0ea
agariss-it-solution <agarissitsolution@gmail.com>
2025-07-04 12:47:11 +0530

    Update Chatbot.js

--- frontend/src/components/Chatbot/Chatbot.js
+++ frontend/src/components/Chatbot/Chatbot.js
@@ -25,7 +25,7 @@ const Chatbot = () => {
         setInput("");
 
         try {
-            const response = await fetch("http://localhost:3001/v1/chat", {
+            const response = await fetch("http://31.97.63.245:3001/v1/chat", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
