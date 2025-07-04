e4748f6059ca2f67ce9634423fe69593787b0c6b
agariss-it-solution <agarissitsolution@gmail.com>
2025-07-04 12:45:50 +0530

    Update ViewDetails.js

--- frontend/src/components/Api/ViewDetails.js
+++ frontend/src/components/Api/ViewDetails.js
@@ -1,4 +1,4 @@
-const BLOG_DETAIL_API = "http://localhost:3001/v1/blogdata";
+const BLOG_DETAIL_API = "http://31.97.63.245:3001/v1/blogdata";
 
 // Corrected:
 export async function fetchProjectData(id) {
