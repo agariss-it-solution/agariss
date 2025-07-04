1338b4395d367b169f11632d622bd15d763f1cd2
agariss-it-solution <agarissitsolution@gmail.com>
2025-07-04 17:54:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,16 +9,12 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors({
-  origin: ['https://agarissitsolution.in', 'http://localhost:3000'], // allow frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(helmet());
 
 // Handle large uploads (fixes 413 Request Entity Too Large)
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(express.json());
+app.use(express.urlencoded());
 
 // Serve uploads folder statically
 app.use("/uploads", express.static("uploads"));
