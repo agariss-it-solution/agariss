08242a6b9bbdf4550e5d07849bd724c0272a12a5
agariss-it-solution <agarissitsolution@gmail.com>
2025-07-04 15:17:39 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,10 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: 'https://agarissitsolution.in' }));
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Serve uploads folder statically
 
